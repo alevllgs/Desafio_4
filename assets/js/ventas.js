166236fa0286dd6035905f97993431e3fce54a04
alevllgs <ale.villegas@uc.cl>
2024-01-11 07:38:26 -0300

    actualiza

---
+++ assets/js/ventas.js
@@ -0,0 +1,47 @@
+
+
+const propiedades_venta = [
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    },
+
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+    }
+]
